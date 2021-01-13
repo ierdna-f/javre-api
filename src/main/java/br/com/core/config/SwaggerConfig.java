9bc0eb1ac3d7ab1feb4ef80ad72d267754a074ee
Andrei Figueiredo <b.andrei.fig@gmail.com>
2021-01-13 16:54:57 -0300

    Adicionando classe de configuração do Swagger para habilitar a documentação

---
+++ src/main/java/br/com/core/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package br.com.core.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(info());
+    }
+    private ApiInfo info(){
+        return new ApiInfo(
+                "Cliente API",
+                "API REST para cadastro de clientes.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Andrei Figueiredo",
+                        "https://www.linkedin.com/in/andrei-figueiredo-289b97115/",
+                        "b.andrei.fig@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/license.html", new ArrayList<>()
+        );
+    }
+}
